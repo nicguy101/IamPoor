9bf16120eb739efdca7e11dea5f73b9dc33d4632
Moyo Ososami <moyoo2@109d25lx1s1f8j7.student.cssd.ab.ca>
2019-02-06 14:14:47 -0700

    Initial Commit

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Moyo Ososami on 2019-02-06.
+//  Copyright © 2019 Moyo Ososami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
